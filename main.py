b70cccb7d1d9cc11274f0628b982a705aa424ce7
Lucas Fita Kabisi <fitakabisil@educ.college-montreal.qc.ca>
2023-01-17 10:46:39 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import random
+
+
+lancer_dé = random.sample((0, 7), 4)
+
+
+class NPC():
+    def __int__(self):
+        self.Force =
+        self.Agilité =
+        self.Constitution =
+        self.Intelligence =
+        self.Sagesse =
+        self.Charisme =
+        self.armure = random.randint(1, 12)
+        self.points_vie = random.randint(1, 20)
