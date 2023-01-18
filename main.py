1d7265ce89a44bcead5e400e4e33324b8f46f965
fitakabisil <112487979+fitakabisil@users.noreply.github.com>
2023-01-17 21:02:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-import random
+lancer_dé6.remove(min(lancer_dé6))
+Sum6 = sum(lancer_dé6)
 
 
-lancer_dé = random.sample((0, 7), 4)
+class NPC:
 
-
-class NPC():
     def __int__(self):
-        self.Force =
-        self.Agilité =
-        self.Constitution =
-        self.Intelligence =
-        self.Sagesse =
-        self.Charisme =
-        self.armure = random.randint(1, 12)
+
+        self.Force = Sum1
+
+        self.Agilité = Sum2
+
+        self.Constitution = Sum3
+
+        self.Intelligence = Sum4
+
+        self.Sagesse = Sum5
+
+        self.Charisme = Sum6
+
+        self.class_armure = random.randint(1, 12)
+
         self.points_vie = random.randint(1, 20)
+
+        self.race = 'Human'
+
+        self.profession = 'fermier'
+
+    def afficher_stats(self):
+
+         print('stats NPC:')
+         print('Force:', self.Force)
+         print('Agilité:', self.Agilité)
+         print('Constitution:', self.Constitution)
+         print('Intelligence:', self.Intelligence)
+         print('Sagesse:', self.Sagesse)
+         print('Charisme:', self.Charisme)
+         print("Classe de l'armure:", self.class_armure)
+         print('Race:', self.race)
+         print('Profession:', self.profession)
+
+
+NPC().afficher_stats()
